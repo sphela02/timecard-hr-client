82db7557ca9173e9f1bd0a7729cd7fda342da5c2
Christian Loris <cloris@harris.com>
2018-01-25 11:59:32 -0500

    TC-0: Added Travis yml file.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'tc'`, async(() => {
+  it(`should have as title 'timecard'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tc');
+    expect(app.title).toEqual('TimeCard');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tc!');
+    expect(compiled.querySelector('h1').textContent).toContain('Timecard App!!!');
   }));
 });
