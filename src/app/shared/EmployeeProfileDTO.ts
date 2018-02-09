86aa1a595524572b20cc5172e5f8a1596974eeae
Phelan <sphela02@harris.com>
2018-02-09 11:10:18 -0500

    20180209 ... Created individual files for each interface and enum, generated directly from Visual Studio. Declaration merge headers put on top, and a tslint rule change to prevent TSLint errors.

---
+++ src/app/shared/EmployeeProfileDTO.ts
@@ -0,0 +1,23 @@
+﻿export interface EmployeeProfileDTO         {}
+export interface NoteDTO                    {}
+export interface TimecardDaysDTO            {}
+export interface TimecardDTO                {}
+export interface TimecardHeaderDTO          {}
+export interface TimecardRowDTO             {}
+export interface TimecardSearchDTO          {}
+export interface TRCCodesDTO                {}
+export interface ValidateProjectActivityDTO {}
+export interface ValidateShiftDTO           {}
+export interface ValidationResponseDTO      {}
+
+export enum HourType         {}
+export enum ValidationStatus {}
+export enum TimecardStatus   {}
+
+
+export interface EmployeeProfileDTO {
+        EMPLID: string;
+        ECAID: string;
+        FullNamePrintable: string;
+        BusinessUnit: string;
+    }
---
+++ src/app/shared/HourType.ts
@@ -0,0 +1,22 @@
+﻿export interface EmployeeProfileDTO         {}
+export interface NoteDTO                    {}
+export interface TimecardDaysDTO            {}
+export interface TimecardDTO                {}
+export interface TimecardHeaderDTO          {}
+export interface TimecardRowDTO             {}
+export interface TimecardSearchDTO          {}
+export interface TRCCodesDTO                {}
+export interface ValidateProjectActivityDTO {}
+export interface ValidateShiftDTO           {}
+export interface ValidationResponseDTO      {}
+
+export enum HourType         {}
+export enum ValidationStatus {}
+export enum TimecardStatus   {}
+
+
+    export enum HourType {
+        Base = 0,
+        Benefit = 1,
+        Other = 2,
+    }
