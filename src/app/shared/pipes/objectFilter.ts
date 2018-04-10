5fd95f5972824cccedb496ecaadba62f2c691fa4
Culver <jculve01@harris.com>
2018-04-10 09:02:24 -0400

    TC: Created piped project list dropdown.

---
+++ src/app/shared/pipes/objectFilter.ts
@@ -0,0 +1,24 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'ObjectFilter',
+})
+export class ObjectFilter implements PipeTransform {
+    transform(value: any, input: string, searchableList: any) {
+        if (input) {
+            input = input.toLowerCase();
+            return value.filter(function (el: any) {
+                let isTrue = false;
+                for ( const k of Object.keys(searchableList) ) {
+                    if (el[searchableList[k]].toLowerCase().indexOf(input) > -1) {
+                        isTrue = true;
+                    }
+                    if (isTrue) {
+                        return el;
+                    }
+                }
+            });
+        }
+        return value;
+    }
+}
