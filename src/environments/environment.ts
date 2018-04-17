535820da67aa08a83c084bfafd2738e00b502f19
Phelan <sphelan@harris.com>
2018-04-17 09:40:24 -0400

    20180416 ... Production deployment work.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,10 @@ import { AppMode } from '../app/shared/shared';
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+// PRODUCTION BUILD STEPS FOR DEV:
+// Map T:\ to \\mlbmblwebd1.cs.myharris.net\e$\netroot\mi-dev\wwwroot
+// Run the following ...
+// ng build --prod --output-path=T:\TimecardClient --base-href=/TimecardClient/
 
 export const environment = {
   production: false,
