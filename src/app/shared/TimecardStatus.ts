e15b99d282af47db870f0c1229f8e8ed430ed510
Phelan <sphelan@harris.com>
2018-05-07 15:00:28 -0400

    20180507 ... Update timecard status DTO and revised isActiveLaborCorrection() to compute correctly (pending/open and isLaborCorrection)

--- src/app/shared/TimecardStatus.ts
+++ src/app/shared/TimecardStatus.ts
@@ -7,5 +7,11 @@ export enum TimecardStatus {
     Pending = 2,
     Approved = 3,
     Future = 4,
+    LOA = 5,
+    NoTimeReported = 6,
+    PreviousBU = 7,
+    Terminated = 8,
+    RecordOnly = 9,
+    InformationOnly = 10,
 }
 
