7f24cab2537e06ac8e03657bde4de0c0e3be0189
Christian Loris <cloris@harris.com>
2018-05-08 11:54:47 -0400

    TC-0: Update packe scripts and enivronments.

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,19 @@
+import { AppMode } from '../app/shared/shared';
+
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// PRODUCTION BUILD STEPS FOR DEV:
+// Map T:\ to \\mlbmblwebd1.cs.myharris.net\e$\netroot\mi-dev\wwwroot
+// Run the following ...
+// ng build --prod --output-path=T:\TimecardClient --base-href=/TimecardClient/
+// BUILD COMMAND FOR GENERATING THE TEST CLIENT (for the .net API service project)
+// (Assuming that the service root is ..\TimecardService\)
+// ng build --environment=local --output-path=..\TimecardService\Timecard.TestClient --base-href=/
+
+export const environment = {
+  production: false,
+  apiServiceURL: 'https://mi-dev.harris.com/test/timecardapi/api/v1/',
+  AppMode: AppMode.Dev
+};
