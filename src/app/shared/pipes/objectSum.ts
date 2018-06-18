0075d4f9780e08e1eccbd4be50bd4ba21159116e
Phelan <sphelan@harris.com>
2018-06-18 17:02:36 -0400

    20180618 ... Added ObjectSum filter to sum up an array of objects based on a particular field.

---
+++ src/app/shared/pipes/objectSum.ts
@@ -0,0 +1,16 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'ObjectSum',
+    pure: true,
+})
+export class ObjectSum implements PipeTransform {
+    transform(values: any[], fieldName: string) {
+        let returnValue: number = 0;
+        values.forEach(value => {
+            returnValue += value[fieldName];
+        });
+        // If input is an string filter list on string value.
+        return returnValue;
+    }
+}
