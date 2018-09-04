0dd5a7eaa659154e19780f0fb5c26d82479c8a0b
Phelan <sphelan@harris.com>
2018-09-04 14:12:47 -0400

    20180830 ... Added /approvals app-level route, using the app landing component and the new approvals menu, and support from the TC and VRS services.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserProfileComponent } from './shared/user-profile/user-profile/user-profile.component';
+import { AppLandingComponent } from './shared/app-landing/app-landing.component';
+import { ApplicationMenuType } from './shared/shared';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: UserProfileComponent
+  },
+  {
+    path: 'approvals',
+    component: AppLandingComponent,
+    data: [{appMenuType: ApplicationMenuType.ApprovalMenu}],
   },
   // { path: '', redirectTo: 'timecards', pathMatch: 'full'},
   // { path: '**', redirectTo: 'timecards', pathMatch: 'full'},
--- src/app/shared/app-landing/app-landing.component.ts
+++ src/app/shared/app-landing/app-landing.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationMenuType, ApplicationMenuItem } from '../shared';
+import {
+  ActivatedRoute,
+  Router
+} from '@angular/router';
+import { CommonDataService } from '../common-data/common-data.service';
 
 @Component({
   selector: 'tc-app-landing',
-  templateUrl: './app-landing.component.html',
-  styleUrls: ['./app-landing.component.scss']
+  template: ''
 })
 export class AppLandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _commonDataService: CommonDataService,
+  ) { }
 
   ngOnInit() {
-  }
 
-}
+    // If we're coming in with a menu type, redirect to the first item in that menu
+    this._route.params.subscribe(newParams => {
+      const appMenuType: ApplicationMenuType = (this._route.snapshot.data[0]) ? this._route.snapshot.data[0]['appMenuType'] : null ;
+      if (appMenuType) {
+        this._commonDataService.getMenu(appMenuType).subscribe((appMenu: ApplicationMenuItem[]) => {
+          // If there's a menu with items, go to the first one
+          if (appMenu) {
+            if (appMenu.length) {
+              this._router.navigate([appMenu[0].path]);
+            } else {
+              // No items for this menu, go back to root.
+              this._router.navigate(['/']);
+            } // end if menu length
+          } else {
+              // Menu doesn't exist, go back to root.
+              this._router.navigate(['/']);
+          } // end if menu exists
+        }); // end subscribe getmenu
+      } // end if app menu type
+    }); // end subscribe route params
+
+  } // end ngOnInit
+
+} // end AppLandingComponent
