7c2cff58fb8c82381bf78ca75c984b8ac8b6bea8
Phelan <sphelan@harris.com>
2018-09-11 17:01:40 -0400

    20180911 ... Fixed AppLandingComponent to stop controlling navigation after it completes its work.

--- src/app/shared/app-landing/app-landing.component.ts
+++ src/app/shared/app-landing/app-landing.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApplicationMenuType, ApplicationMenuItem } from '../shared';
 import {
   ActivatedRoute,
   Router
 } from '@angular/router';
 import { CommonDataService } from '../common-data/common-data.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'tc-app-landing',
   template: ''
 })
-export class AppLandingComponent implements OnInit {
+export class AppLandingComponent implements OnInit, OnDestroy {
+
+  stopListeningToAll$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private _route: ActivatedRoute,
@@ -21,10 +24,10 @@ export class AppLandingComponent implements OnInit {
   ngOnInit() {
 
     // If we're coming in with a menu type, redirect to the first item in that menu
-    this._route.params.subscribe(newParams => {
+    this._route.params.takeUntil(this.stopListeningToAll$).subscribe(newParams => {
       const appMenuType: ApplicationMenuType = (this._route.snapshot.data[0]) ? this._route.snapshot.data[0]['appMenuType'] : null ;
       if (appMenuType) {
-        this._commonDataService.getMenu(appMenuType).subscribe((appMenu: ApplicationMenuItem[]) => {
+        this._commonDataService.getMenu(appMenuType).takeUntil(this.stopListeningToAll$).subscribe((appMenu: ApplicationMenuItem[]) => {
           // If there's a menu with items, go to the first one
           if (appMenu) {
             if (appMenu.length) {
@@ -42,5 +45,9 @@ export class AppLandingComponent implements OnInit {
     }); // end subscribe route params
 
   } // end ngOnInit
+
+  ngOnDestroy(): void {
+    this.stopListeningToAll$.next(true);
+  }
 
 } // end AppLandingComponent
