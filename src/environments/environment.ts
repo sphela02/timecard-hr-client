ecfed4d715e6ae269fd86a82020703857f86be70
Phelan <sphelan@harris.com>
2019-07-03 12:36:13 -0400

    20190703 ... INITIAL CREATION OF HARRIS MOBILE APP FRAMEWORK APP SHELL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
 import { AppMode, ApplicationEnvironment } from '../app/shared/shared';
-import { VacationRequestModule } from '../app/vacation-request/vacation-request.module';
-import { EmployeeSelfServiceModule } from '../app/employee-self-service/employee-self-service.module';
-import { TimecardModule } from '../app/timecard/timecard.module';
 
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
@@ -22,16 +19,10 @@ export const dbgApiServiceURLGetEmployeeProfile = 'https://mi-dev.harris.com/dev
 export const environment: ApplicationEnvironment = {
   production: false,
   apiServiceURLs: {
-    'VRS':      'https://mi-dev.harris.com/DEV/VRSAPI/api/v1/Vacation/',
-    'TIMECARD': 'https://mi-dev.harris.com/dev/timecardapi/api/v1/Timecard/',
     'EMPLOYEE': 'https://mi-dev.harris.com/dev/timecardapi/api/v1/Employee/',
-    'ESS':      'https://mi-dev.harris.com/dev/EmpSelfServiceAPI/api/v1/EmployeeSelfService/',
   },
   AppMode: AppMode.Dev,
   allowDiagnostics: true,
   importModules: [
-    VacationRequestModule,
-    EmployeeSelfServiceModule,
-    TimecardModule,
   ],
 };
